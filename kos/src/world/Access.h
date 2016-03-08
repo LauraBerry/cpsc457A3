372ce1f29281049e9469adc61a8f0b22c1b37be4
lnberry <lnberry@zone07-wa.cs.ucalgary.ca>
2016-03-08 15:16:18 -0700

    still fighting with savedMemory but this version runs at least

--- kos/src/world/Access.h
+++ kos/src/world/Access.h
@@ -61,7 +61,7 @@ extern map<string,RamFile> kernelFS;
 
 
 extern map<string,MyRamFile> myKernelFS;
-extern string* savedMemory;
+extern char* savedMemory;
 //initalize savedMemory here so that it is accessible where ever myKernelFS is accessable
 
 class FileAccess : public Access {
